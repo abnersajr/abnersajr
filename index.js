1cd4e35f9ec24e7ad9fa040b47940c3fe60447db
Abner Soares Alves Junior <abnersajr@gmail.com>
2022-11-18 01:26:21 -0500

    feat: first version

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+console.log(`
+Abner Soares Alves Junior
+
+Software Engineer from 🇧🇷
+Currently living in Montreal 🇨🇦
+and working at Busbud: 🚌 https://busbud.com
+
+😸 Github: https://github.com/abnersajr
+`)
