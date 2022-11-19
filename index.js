ca2f8bdc2b578b700f4d6346fbc55a76c27c070c
Abner Soares Alves Junior <abnersajr@gmail.com>
2022-11-19 18:59:59 -0500

    feat: adds cli-welcome package

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
+const pkgJSON = require("./package.json");
+const welcome = require("cli-welcome");
 
-const clearAnyConsole = require("clear-any-console")
-
-clearAnyConsole();
+welcome({
+  title: pkgJSON.name,
+  tagLine: `Hey, nice to meet you!`,
+  description: pkgJSON.description,
+  version: pkgJSON.version,
+  bgColor: `#fc6203`,
+  color: `#000000`,
+  bold: true,
+  clear: false
+})
 
 console.log(`
 Abner Soares Alves Junior
