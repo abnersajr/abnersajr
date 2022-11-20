dacdd1db0ebbc2d0d8d9c096ca8740a02ef7ec1f
Abner Soares Alves Junior <abnersajr@gmail.com>
2022-11-20 01:07:25 -0500

    feat: get followers from GH api

--- utils/stats.js
+++ utils/stats.js
@@ -1,12 +1,18 @@
-const fetch = require("node-fetch");
+const alert = require("cli-alerts");
+const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
-const apiURL = `https://api.github.com/users/abnersajr`;
+const apiURL = `https:/xapi.github.com/users/abnersajr`;
 module.exports = async () => {
-	const res = await fetch(apiURL);
-	const data = await res.json();
-	const ghFollowers = data.followers;
+	try {
+		const res = await fetch(apiURL);
+		if (res.status !== 200) throw res;
 
-	console.log(`
-  Github Followers: ${ghFollowers}
+		const data = await res.json();
+		const ghFollowers = data.followers;
+		console.log(`
+Github Followers: ${ghFollowers}
   `);
+	} catch (error) {
+		alert({ type: "error", msg: error.status || error });
+	}
 };
