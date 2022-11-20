3d960a6b34c4ea68d213310ac014920fe438557c
Abner Soares Alves Junior <abnersajr@gmail.com>
2022-11-19 19:37:38 -0500

    feat: adds colors using Chalk

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 #!/usr/bin/env node
 const pkgJSON = require("./package.json");
 const welcome = require("cli-welcome");
+const chalk = require("chalk");
 
 welcome({
   title: pkgJSON.name,
   tagLine: `Hey, nice to meet you!`,
   description: pkgJSON.description,
   version: pkgJSON.version,
-  bgColor: `#fc6203`,
+  bgColor: `#F4772A`,
   color: `#000000`,
   bold: true,
   clear: false
 })
 
+const bbColor = chalk.bgHex(`#FFAB00`).hex(`#000`);
+const nameColor = chalk.bgHex(`#F4772A`).hex('#000')
+const githubColor = chalk.bgHex(`#6cc644`).hex('#000');
 console.log(`
-Abner Soares Alves Junior
+${nameColor.bold(`Abner Soares Alves Junior`)}
 
+${chalk(`
 Software Engineer from 🇧🇷
 Currently living in Montreal 🇨🇦
-and working at Busbud: 🚌 https://busbud.com
-
-😸 Github: https://github.com/abnersajr
+and working at ${bbColor.bold(` Busbud `)}: 🚌 https://busbud.com
+`)}
+😸 ${githubColor.bold(` Github `)}: ${chalk(`https://github.com/abnersajr`)}
 `)
